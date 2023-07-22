520e94db75e55210bb561866b9f749d3ad8a6a08
Ryo Kuroyanagi <im.ryokuro@gmail.com>
2023-07-22 21:06:07 +0900

    added json output

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import cv2
 from rembg import remove
 from PIL import Image, ImageOps
+import json
 
 print("OpenCV version: " + cv2.__version__)
 
@@ -9,13 +10,14 @@ resized_image_path = 'output/resized_image.png'
 bg_removed_path = 'output/bg_removed.png'
 bw_path = 'output/bw.png'
 contours_path = 'output/contours.png'
+largest_contour_json_path = 'output/contour.json'
 
 # Open an image file
 img = cv2.imread(original_image_path)
 width, height = img.shape[:2]
 print("image size: ", width, height)
 
-max_size = 300
+max_size = 100
 scaling_factor = max_size / max(width, height)
 
 img_resized = cv2.resize(
@@ -48,6 +50,45 @@ cv2.imwrite(contours_path, bw_img)
 print("Number of contours found = ", len(contours))
 for contour in contours:
   print("Points in contour: ", len(contour))
+
+largest_contour_index = -1
+max_area = 0
+for i in range(len(contours)):
+  min_x = 1000000
+  min_y = 1000000
+  max_x = 0
+  max_y = 0
+  for point in contours[i]:
+    x = point[0][0]
+    y = point[0][1]
+    if x < min_x:
+      min_x = x 
+    if y < min_y:
+      min_y = y
+    if x > max_x:
+      max_x = x
+    if y > max_y:
+      max_y = y
+  # Removing contours that are on the edge of the image.
+  if min_x < max_size / 100 or min_y < max_size / 100 or max_x > width - max_size / 100 or max_y > height - max_size / 100:
+    continue
+  area = (max_x - min_x) * (max_y - min_y)
+  if area > max_area:
+    max_area = area
+    largest_contour_index = i
+
+if largest_contour_index == -1:
+  raise Exception("No largest contour found")
+
+largest_countour = contours[largest_contour_index]
+data = {}
+points = []
+data["points"] = points
+for point in largest_countour:
+  points.append([float(point[0][0]), float(point[0][1])])
+
+with open(largest_contour_json_path, 'w') as outfile:
+  outfile.write(json.dumps(data))
 
 cv2.imshow('Image with contours', bw_img)
 cv2.waitKey(0)
